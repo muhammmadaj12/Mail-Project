98961089c2c351210629ac043c98b35fbb58f6c2
muhammmadaj12 <buttgreat543@gmail.com>
2023-12-13 13:14:26 +0500

    Laravel site

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class ContactController extends Controller
+{
+    public function sendEmail(Request $request)
+    {
+
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'phone' => 'required|string',
+            'message' => 'required|string',
+        ]);
+
+        // Send email
+        $data = [
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'phone' => $request->input('phone'),
+            'message' => $request->input('message'),
+        ];
+
+        Mail::to('buttgreat543@email.com')->send(new \App\Mail\ContactMail($data));
+
+        return response()->json(['message' => 'Email sent successfully'], 200);
+    }
+}
+
